823cfc258fa4f4bdb919a4363483c96ee93eb752
darynarodicheva <daryna.r@gowombat.team>
2023-06-05 15:53:15 +0300

    feat: add doc reader

---
+++ src/main/java/DocxReader.java
@@ -0,0 +1,27 @@
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class DocxReader {
+    public String readDocxFile(String filePath) {
+        StringBuilder textBuilder = new StringBuilder();
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             XWPFDocument document = new XWPFDocument(fis)) {
+
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+            for (XWPFParagraph paragraph : paragraphs) {
+                String text = paragraph.getText();
+                textBuilder.append(text).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return textBuilder.toString();
+    }
+}
